628e1acb16c1f4386b9787d883a6b5e501bf914b
Sugira Elisha <sugirangendahayo835@gmail.com>
2026-03-11 14:32:37 +0200

    Created reusable component and rendered it

--- src/test.jsx
+++ src/test.jsx
@@ -1,15 +1,40 @@
-import { createRoot } from "react-dom/client";
+Part 2:
+- Add a `<header>` element with an `<img />` element with the image of the
+  React logo inside (src="react-logo.png") and make sure to set the
+  width to something more manageable so it doesn't take up the whole screen.
+  Also, as always, you should include some alt text on the image.
+- Add an `<h1>` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list, then wrap
+  the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
+  structure flowing well.
+- Add a `<footer>` after the list that says:
+    "© 20xx <last name here> development. All rights reserved."
+ */
 
-createRoot(document.getElementById("root")).render(
-  <div>
-    <img src="./src/assets/react-logo.png" width="40px" />
-    <h1>Func facts about react!</h1>
-    <ul>
-      <li>Was first release in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 200K stars on GitHub</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>,
-);
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+  function Page() {
+      return (
+          <div>
+              <header>
+                  <img src="react-logo.png" width="40px" alt="React logo" />
+              </header>
+              <main>
+                  <h1>Reason I am excited to learn React</h1>
+                  <ol>
+                      <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                      <li>I am more likely to get a job as a front end developer if I know React</li>
+                  </ol>
+              </main>
+              <footer>
+                  <small>© 2024 Ziroll development. All rights reserved.</small>
+              </footer>
+          </div>
+      )
+  }
+
+
+root.render(
+<Page />
+)
