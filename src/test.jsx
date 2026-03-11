68d0168738d23df237f77f23b9b6051d757df79d
Sugira Elisha <sugirangendahayo835@gmail.com>
2026-03-11 13:17:26 +0200

    Removes empty index file
    Cleans up the project structure by deleting an unused and empty entry point file.

---
+++ src/test.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <div>
+    <img src="./src/assets/react-logo.png" width="40px" />
+    <h1>Func facts about react!</h1>
+    <ul>
+      <li>Was first release in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 200K stars on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>,
+);
