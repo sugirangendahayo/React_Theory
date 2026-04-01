73d73d0a6e7bc8811cd977da30023791e095c6e0
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-01 14:24:28 +0200

    feat: add UserRow component to display user information in a formatted layout

--- src/components/ui/UserRow.jsx
+++ src/components/ui/UserRow.jsx
@@ -3,10 +3,20 @@ import React from "react";
 const UserRow = ({ name, country, score }) => {
   return (
     <>
-      <div className="flex flex-col gap-4 justify-center items-center">
-        <span>{name}</span>
-        <span>{country}</span>
-        <span>{score}</span>
+      <div className="flex flex-row gap-4 justify-center items-center m-2 bg-gray-600 text-white">
+        <div className="flex flex-col justify-center items-center">
+          <span className="text-cyan-500 p-2">Name</span>
+          <span>{name}</span>
+        </div>
+         <div className="flex flex-col justify-center items-center">
+          <span className="text-cyan-500 p-2">Country</span>
+          <span>{country}</span>
+        </div>
+         <div className="flex flex-col justify-center items-center">
+          <span className="text-cyan-500 p-2">Score</span>
+
+          <span>{score}</span>
+        </div>
       </div>
     </>
   );
