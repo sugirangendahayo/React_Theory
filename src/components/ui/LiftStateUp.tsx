79203d12198327f79197408483f7b95734d94306
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-01 12:55:51 +0200

    feat: add LiftStateUp component to demonstrate state lifting pattern

--- src/components/ui/LiftStateUp.tsx
+++ src/components/ui/LiftStateUp.tsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const LiftStateUp = () => {
-    const 
+  const [text, setText] = useState("");
   return (
-    
-  )
-}
+    <>
+      <Input text={text} setText={setText} />
+      <Preview  text={text}/>
+    </>
+  );
+};
 
-export default LiftStateUp
+export default LiftStateUp;
 
 function Input({ text, setText }) {
-  return (
-    <input
-      value={text}
-      onChange={(e) => setText(e.target.value)}
-    />
-  );
+  return <input value={text} onChange={(e) => setText(e.target.value)} />;
 }
 
 function Preview({ text }) {
